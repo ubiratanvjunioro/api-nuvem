541d2ded3328a999ebd8c76bb7b93a735ab709fe
Ubiratanvjunior <164281522+ubiratanvjunioro@users.noreply.github.com>
2026-03-16 12:46:58 +0000

    corrigindo cors

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.json({
     mensagem: "API funcionando na NUVEM 🚀",
     status: "online"
   });
 });
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
